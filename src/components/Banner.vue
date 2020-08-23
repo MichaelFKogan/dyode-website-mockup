588274ad1cf210cdf32d03c82f872f43b137a784
mfkogan <mfkogan@gmail.com>
2020-08-24 03:02:38 +0700

    added text to hero baner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,11 +1,19 @@
 <template>
 
+<div>
 
 <div class="hero-banner">
 	<img class="hero-banner-image" :src="require('../assets/img/Hero-Image-1.jpg')" />
 </div>
 
+<div class="hero-inner-text-left">
+<div class="hero-title">Title Goes Here</div>
+<div class="hero-tagline">Tagline will go right here.</div>
+<div class="hero-button-container"><button class="hero-button">SHOP NOW</button></div>
+</div>
 
+
+</div>
 
 </template>
 
