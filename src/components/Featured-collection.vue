bc866f92e4df2653be0784bff9d5391e54f19d92
mfkogan <mfkogan@gmail.com>
2020-08-24 12:51:26 +0700

    adjusting featured-collection section

--- src/components/Featured-collection.vue
+++ src/components/Featured-collection.vue
@@ -2,6 +2,40 @@
 
 <div>
 
+
+	<div class="featured-collection-wrapper">
+		<div class="feature-title">New Arrivals</div>
+
+				<div class="">
+					<img class="" :src="require('../assets/img/Arrow-Left.png')" />
+				</div>
+
+				<div class="featured-collection-image-wrap">
+					<img class="featured-collection-image" :src="require('../assets/img/Product-Image-1.jpg')" />
+					<div class="product-title">Product Title</div>
+					Women's T-Shirt
+					$19.99 
+				</div>
+
+
+				<div class="featured-collection-image-wrap">
+					<img class="featured-collection-image" :src="require('../assets/img/Product-Image-1.jpg')" />
+				</div>
+
+				<div class="featured-collection-image-wrap">
+					<img class="featured-collection-image" :src="require('../assets/img/Product-Image-1.jpg')" />
+				</div>
+
+
+				<div class="featured-collection-image-wrap">
+					<img class="featured-collection-image" :src="require('../assets/img/Product-Image-1.jpg')" />
+				</div>		
+
+				<div class="">
+					<img class="" :src="require('../assets/img/Arrow-Right.png')" />
+				</div>		
+	</div>
+
 </div>
 
 </template>
